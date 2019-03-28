e37370f6c6ac3de7d9b3b25075db4f5923525092
Dan Ross <daniellross@gmail.com>
2019-03-27 22:50:04 -0400

    started on JS function. added header and will subdivid index.html into individual pages for each function

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
-//fizz buzz question solution
+const checkerNumber = document.querySelector('checker--number')
+const checkerResult = document.querySelector('checker--result')
+
+const numberSubmitChecker =  document.querySelector('numberSubmit--checker')
+
+const numberFieldChecker = document.querySelector('numberField--checker')
+//let resetButton; (not sure if this is needed for mine? check MDN page)
+
+console.log('This Works')
+
+numberSubmitChecker.onclick = function(){
+
+}
+
+
+//this is my fizz buzz question solution
 // for (i = 1; i < 100; i++){
 //     if (i%3 == 0 && i%5 == 0){
 //         console.log('fizzbuzz')
@@ -8,4 +23,3 @@
 //         console.log('fizz')
 // }else console.log(i)
 // } 
-
