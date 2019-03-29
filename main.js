26352f5d93655192243421f6bf15c6a04952a8a6
Dan Ross <daniellross@gmail.com>
2019-03-28 20:57:54 -0400

    more experiments in JS on evenReturn

--- main.js
+++ main.js
@@ -1,21 +1,40 @@
-const checkerNumber = document.getElementsByClassName('checker--number')
-const checkerResult = document.getElementsByClassName('checker--result')
+//even checker works in browser
+// const evenCheck = function (happy){
+//     if (happy%2 == 0)
+//         console.log('even number')
+// else if (happy%2 != 0)
+//     console.log('not even')
+// }
 
-const checkerSubmit =  document.getElementsByClassName('checker--submit')
+const groupOne = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 10]
 
-const checkerValue = document.getElementsByClassName('checker--value')
-//let resetButton; (not sure if this is needed for mine? check MDN page)
+//function to return even numbers from array
+const evenReturn = function (groupOne){
+    for (var i = 0; i < groupOne.length; i++){
+    }
+    if (i%2 == 0){
+        console.log(i)
+    } 
+};
 
-console.log('This Works')
+//function to add only odd numbers from array
+const oddSum = function (groupTwo){
 
-checkerSubmit.addEventListener = ('click', console.log('this button works'));
-
-const evenCheck = function (happy){
-    if (happy%2 == 0)
-        console.log('even number')
-else if (happy%2 != 0)
-    console.log('not even')
 }
+
+// these are constants to go with the functional site
+// const checkerNumber = document.getElementsByClassName('checker--number')
+// const checkerResult = document.getElementsByClassName('checker--result')
+
+// const checkerSubmit =  document.getElementsByClassName('checker--submit')
+
+// const checkerValue = document.getElementsByClassName('checker--value')
+// //let resetButton; (not sure if this is needed for mine? check MDN page)
+
+// console.log('This Works')
+
+// checkerSubmit.addEventListener = ('click', console.log('this button works'));
+
 
 //this is my fizz buzz question solution
 // for (i = 1; i < 100; i++){
